38bfd7f55e32e5eb8682272c8f3727f315b3fd39
sween <sweenmarygeorge@gmail.com>
2024-01-11 00:55:03 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import QuoteGenerator from './QuoteGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Random Quotes Generator</h1>
+      <QuoteGenerator />
+    </div>
+  );
+}
+
+export default App;
